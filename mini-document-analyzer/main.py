825daafdef127878348b286aee78726075521a87
kostya-yesypenko <kostaesipenko120@email.com>
2026-03-05 22:19:09 +0100

    add CLI argument parsing

--- mini-document-analyzer/main.py
+++ mini-document-analyzer/main.py
@@ -2,5 +2,6 @@ from cli import run_cli
 
 if __name__ == "__main__":
     
-
+    print("""Welcome to the Mini Document Analyzer CLI!
+          Usage: python main.py <input_file> [output_file]""")
     run_cli()
