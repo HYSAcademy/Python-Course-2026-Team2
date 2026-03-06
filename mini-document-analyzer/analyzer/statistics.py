717fa41951b17556cde558dca4d3d5fe624c94ea
Kateryna <fish4715@gmail.com>
2026-03-06 17:36:16 +0200

    Add function that returns top 10 frequently used words

--- mini-document-analyzer/analyzer/statistics.py
+++ mini-document-analyzer/analyzer/statistics.py
@@ -11,3 +11,8 @@ def count_word_frequencies(words: list[str]):
         word_frequencies[word] = word_frequencies.get(word, 0) + 1
 
     return word_frequencies
+
+def get_top_words(frequencies: dict[str, int], quantity: int = 10):
+    sorted_frequencies = sorted(frequencies.items(), key=lambda x: x[1], reverse=True)
+
+    return [list(freq) for freq in sorted_frequencies[:quantity]]
