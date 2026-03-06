207c4ae32ec10647b3b1fc135a74227dc0a7b346
Kateryna <fish4715@gmail.com>
2026-03-06 18:13:33 +0200

    Add data processing logic to cli

--- mini-document-analyzer/cli.py
+++ mini-document-analyzer/cli.py
@@ -1,5 +1,12 @@
 import json
 import os
+from analyzer import read_file, get_modified_data, get_statistics
+from formatter import format_data
+
+content = read_file('path.txt') # Change to input file path
+data = get_modified_data(content)
+statistics = get_statistics(data['tokens'], content)
+output = format_data('path.txt', data, statistics) # Change to input file path
 
 
 def run_cli():
