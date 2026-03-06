6606134f3b9b9b7591526461019df3ff0de58e2d
Kateryna <fish4715@gmail.com>
2026-03-06 17:45:16 +0200

    Add function that returns all statistics

--- mini-document-analyzer/analyzer/statistics.py
+++ mini-document-analyzer/analyzer/statistics.py
@@ -16,3 +16,16 @@ def get_top_words(frequencies: dict[str, int], quantity: int = 10):
     sorted_frequencies = sorted(frequencies.items(), key=lambda x: x[1], reverse=True)
 
     return [list(freq) for freq in sorted_frequencies[:quantity]]
+
+def get_statistics(words: list, raw_text: str):
+    total_sentences = count_sentences(raw_text)
+    word_frequencies = count_word_frequencies(words)
+    top_words = get_top_words(word_frequencies)
+
+    return {
+        'total_chars': len(raw_text),
+        'total_words': len(words),
+        'total_sentences': total_sentences,
+        'word_frequencies': word_frequencies,
+        'top_words': top_words,
+    }
