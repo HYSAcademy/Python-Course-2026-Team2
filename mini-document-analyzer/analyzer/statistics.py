32106a9e9741fb3e1631fbf7f70bd8fd131d52d9
Kateryna <fish4715@gmail.com>
2026-03-06 17:32:41 +0200

    Add counter for word frequencies

--- mini-document-analyzer/analyzer/statistics.py
+++ mini-document-analyzer/analyzer/statistics.py
@@ -3,3 +3,11 @@ def count_sentences(text: str):
     count = sum(1 for char in text if char in punctuation)
 
     return count
+
+def count_word_frequencies(words: list[str]):
+    word_frequencies = {}
+
+    for word in words:
+        word_frequencies[word] = word_frequencies.get(word, 0) + 1
+
+    return word_frequencies
