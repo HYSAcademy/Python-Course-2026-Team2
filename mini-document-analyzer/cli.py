8bfe9d519e53de02b07b33bba9e6ac8348cc60e4
kostya-yesypenko <kostaesipenko120@email.com>
2026-03-06 18:10:43 +0100

    corrected cli argument parsing to use argparse library

--- mini-document-analyzer/cli.py
+++ mini-document-analyzer/cli.py
@@ -1,6 +1,7 @@
 import json
 import sys
 import os
+import argparse
 
 
 def run_cli():
@@ -8,37 +9,46 @@ def run_cli():
     # Expect exactly 1 or 2 arguments after the script name.
     # The first argument is the required input file path, and the second
     # (optional) argument is the desired output file name.
-    if len(sys.argv) < 2:
-        print("Error: Missing input file argument")
-        sys.exit(1)
-    if len(sys.argv) > 3:
-        print("Error: Too many arguments")
-        print("Usage: python -m mini_document_analyzer.cli <input.txt> [output.json]")
-        sys.exit(1)
+    
+    parser = argparse.ArgumentParser(description="Mini Document Analyzer")
 
-    input_file = sys.argv[1]
+    parser.add_argument(
+        "input",
+        help="Path to input TXT file"
+    )
 
-    if not os.path.isfile(input_file) or not input_file.endswith('.txt'):
+    parser.add_argument(
+        "output",
+        nargs="?",  # optional positional argument
+        help="Optional output JSON filename"
+    )
+
+    args = parser.parse_args()
+
+    input_file = args.input
+
+    if not os.path.isfile(input_file) or not input_file.endswith(".txt"):
         print("Error: Input file must be a valid TXT file")
-        sys.exit(1)
-        
+        return
+
     if os.stat(input_file).st_size == 0:
         print(f"Error: {input_file} is empty")
-        sys.exit(1)
-        
-    if len(sys.argv) == 3 and sys.argv[2].endswith('.json'):
-        output_file = sys.argv[2]
+        return
+
+    if args.output:
+        output_file = args.output
     else:
-        output_file = f"{os.path.splitext(input_file)[0]}.analytics.json"
+        output_file = f"{os.path.splitext(input_file)[0]}.analysis.json"
+
     print(f"Input file: {input_file}")
     print(f"Output file: {output_file}")
     
      # --- ANALYSIS PIPELINE ---
+     
 
 
     # --- JSON EXPORT ---
-    
-    
+        
     export_json(output, output_file)
     print("Analysis completed successfully.")
 
@@ -46,7 +56,5 @@ def run_cli():
 def export_json(data,output_file):
     with open(output_file, 'w') as f:
         json.dump(data, f, indent=4)
-
-# Call the main analysis function from the core module and write results to output file.
 
 
