873a2126d86814e1e4fbd3c5fd0945f7f3ff7ee1
Kateryna <fish4715@gmail.com>
2026-03-07 10:18:25 +0200

    Improve code after review

--- mini-document-analyzer/analyzer/statistics.py
+++ mini-document-analyzer/analyzer/statistics.py
@@ -1,10 +1,14 @@
-def count_sentences(text: str):
-    punctuation = ".?!"
-    count = sum(1 for char in text if char in punctuation)
+from typing import Any
+import re
 
-    return count
 
-def count_word_frequencies(words: list[str]):
+def count_sentences(text: str) -> int:
+    s_list = re.split(r"[.?!]+", text)
+    sentences = [s.strip() for s in s_list if s.strip()]
+
+    return len(sentences)
+
+def count_word_frequencies(words: list[str]) -> dict[str, int]:
     word_frequencies = {}
 
     for word in words:
@@ -12,12 +16,12 @@ def count_word_frequencies(words: list[str]):
 
     return word_frequencies
 
-def get_top_words(frequencies: dict[str, int], quantity: int = 10):
+def get_top_words(frequencies: dict[str, int], quantity: int = 10) -> list[list[str | int]]:
     sorted_frequencies = sorted(frequencies.items(), key=lambda x: x[1], reverse=True)
 
     return [list(freq) for freq in sorted_frequencies[:quantity]]
 
-def get_statistics(words: list, raw_text: str):
+def get_statistics(words: list[str], raw_text: str) -> dict[str, Any]:
     total_sentences = count_sentences(raw_text)
     word_frequencies = count_word_frequencies(words)
     top_words = get_top_words(word_frequencies)
