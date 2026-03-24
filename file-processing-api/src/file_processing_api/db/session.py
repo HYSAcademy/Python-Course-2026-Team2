d6105bbb04ebad964513d889669d5dfc333ee808
Kateryna <fish4715@gmail.com>
2026-03-24 15:29:51 +0200

    fix: add upload file optimization

--- file-processing-api/src/file_processing_api/db/session.py
+++ file-processing-api/src/file_processing_api/db/session.py
@@ -3,10 +3,11 @@ from sqlalchemy.orm import sessionmaker
 from .database import engine, sync_engine
 from contextlib import contextmanager
 from sqlmodel import Session
+from sqlalchemy.ext.asyncio import async_sessionmaker
 
 
+AsyncSessionFactory = async_sessionmaker(engine, expire_on_commit=False)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-
 
 async def get_session():
     async with async_session() as session:
@@ -17,3 +18,6 @@ async def get_session():
 def get_sync_session():
     with Session(sync_engine) as session:
         yield session
+
+def get_session_factory():
+    return AsyncSessionFactory
