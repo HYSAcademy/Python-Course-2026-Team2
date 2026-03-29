d6c81081c1dda4bef0cb9ce918f055993be9972b
Kateryna <fish4715@gmail.com>
2026-03-29 22:32:25 +0300

    feat: update redis client

--- file-processing-api/redis_client/client.py
+++ file-processing-api/redis_client/client.py
@@ -2,7 +2,7 @@ import os
 import redis
 
 r = redis.Redis(
-    host=os.getenv("REDIS_HOST", "redis"),
+    host=os.getenv("REDIS_HOST", "redis_client"),
     port=int(os.getenv("REDIS_PORT", 6379)),
     db=0,
     decode_responses=True)
--- file-processing-api/redis_client/publisher.py
+++ file-processing-api/redis_client/publisher.py
@@ -1,5 +1,5 @@
 import json
-from client import r
+from .client import r
 
 def publish_message(channel: str, message: dict):
     r.publish(channel, json.dumps(message))
